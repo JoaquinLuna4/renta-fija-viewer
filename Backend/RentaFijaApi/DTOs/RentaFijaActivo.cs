7d2b1953b2f16e8bfe78a8ef75f7e246d42702db
JoaquinLuna4 <joaquinfluna4@gmail.com>
2025-07-02 18:47:13 -0300

    Se suman git ignore

--- Backend/RentaFijaApi/DTOs/RentaFijaActivo.cs
+++ Backend/RentaFijaApi/DTOs/RentaFijaActivo.cs
@@ -5,7 +5,7 @@
         public string TipoActivo { get; set; } // Ej: "BONTE", "BONAR", "LECAP", "BOPREAL", "ON", "BONOS DE CONSOLIDACIÓN"
         public string NombreCompleto { get; set; } // Nombre largo del activo, ej. "BONAR STEP-UP USD Ley Arg 2029"
         public string CodigoTicker { get; set; } // El código corto, ej. "AL29", "GD30", "TX26"
-        public DateTime? Vencimiento { get; set; } // Podría ser DateTime si lo parseas
+        public string Vencimiento { get; set; } // Podría ser DateTime si lo parseas
         //public string Moneda { get; set; } // ARS, USD, etc. (Si lo extraes)
         //public string Clase { get; set; } // Ley Arg, Ley NY, etc. (Si lo extraes)
         public decimal? Cotizacion { get; set; } // Usamos decimal? para permitir nulos si no siempre está presente
@@ -22,7 +22,7 @@
         // Opcional: Un método ToString() para facilitar la depuración y ver el objeto en consola
         public override string ToString()
         {
-            return $"Tipo: {TipoActivo ?? "N/A"}, Ticker: {CodigoTicker ?? "N/A"}, Nombre: {NombreCompleto ?? "N/A"}, Vto: {Vencimiento?.ToString("dd-MMM-yy") ?? "N/A"}, Cotiz: {Cotizacion?.ToString("F2") ?? "N/A"}, TIR: {TirAnual?.ToString("F2") ?? "N/A"}%, Paridad: {Paridad?.ToString("F2") ?? "N/A"}%";
+            return $"Tipo: {TipoActivo ?? "N/A"}, Ticker: {CodigoTicker ?? "N/A"}, Nombre: {NombreCompleto ?? "N/A"}, Vto: {Vencimiento ?? "N/A"}, Cotiz: {Cotizacion?.ToString("F2") ?? "N/A"}, TIR: {TirAnual?.ToString("F2") ?? "N/A"}%, Paridad: {Paridad?.ToString("F2") ?? "N/A"}%";
         }
     }
 }
