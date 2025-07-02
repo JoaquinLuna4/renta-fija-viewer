11c954a4745fe8db7f61198ef3dcee725df82b6e
JoaquinLuna4 <joaquinfluna4@gmail.com>
2025-07-02 18:50:43 -0300

    Limpia el caché de Git y aplica .gitignore para archivos de compilación y temporales

--- Backend/RentaFijaApi/Services/IPdfExtractionService.cs
+++
@@ -1,9 +0,0 @@
-﻿using RentaFijaApi.DTOs;
-
-namespace RentaFijaApi.Services // Ajusta tu namespace si es diferente
-{
-    public interface IPdfExtractionService
-    {
-        List<RentaFijaActivo> ExtractRentaFijaData(string pdfFilePath);
-    }
-}
